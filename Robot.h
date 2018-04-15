4726cc28558aa87a881f653fa023f1ad4c2cf6f4
waralex26 <38217365+waralex26@users.noreply.github.com>
2018-04-15 21:47:34 +0200

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -25,15 +25,17 @@ class Robot {
 	Sonar sonarF;
 	Sonar sonarFL;
 	Sonar sonarFR;
+	int sonarTurn = 1;
 	long timingSonar;
-	int tSonar = 50;
+	int tSonar = 100;
 	double errorOld = 0;
 	double vPidOld = 0;
 	bool newSonar = false;
-
+	NewPing newSonarR = NewPing(48,49);
+	NewPing newSonarFR = NewPing(46, 47);
 public:
 	Robot(int T = 50, double xPos = 0, double yPos = 0,
-		double omegaPos = 0, int base = 23, int diameter = 6);
+		double omegaPos = 0, int base = 23, int diameter = 9);
 	void countRobot();
 	void goStraight(double desiredSpeed);
 	void stop();
@@ -47,7 +49,9 @@ public:
 	bool getAngle(double omegaDesired);
 	void actuateSonar();
 	void printSonar();
-	void followWallLeft(double desiredDistance, double vBase = 160.0);
+	void followWallRight(double desiredDistance, double vBase = 160.0);
+	void followWallLeftNewPing(double desiredDistance, int actualDistance,
+		double vBase = 100.0);
 };
 
 #endif
