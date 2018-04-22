ed0f07f011f0af8c77c329be794bd0e923795c7a
waralex26 <38217365+waralex26@users.noreply.github.com>
2018-04-22 09:35:28 +0200

    Add files via upload

--- Robot.cpp
+++ Robot.cpp
@@ -25,6 +25,8 @@ Robot::Robot(int T = 50, double xPos = 0, double yPos = 0,
 	motorLeft.setSamplingTime(tSample);
 	motorRight.setSamplingTime(tSample);
 	speedOld = 0;
+
+
 }
 
 void Robot::countRobot()
@@ -74,12 +76,7 @@ void Robot::localisation()
 
 }
 
-void Robot::localisationSonar()
-{
-	x = x + sonarF.getDelta()*cos(omega);
-	y = y + sonarR.getDelta()*cos(omega);
-	omega = atan2(sonarR.getDelta(), sonarF.getDelta());
-}
+
 
 bool Robot::goTo(double xDesired, double yDesired)
 {
@@ -194,112 +191,192 @@ bool Robot::getAngle(double omegaDesired)
 	return isReached;
 }
 
-void Robot::actuateSonar()
+bool Robot::actuateSonar()
 {
 	if (millis() > (timingSonar + tSonar))
 	{
+		timingSonar += tSonar;
 		if (sonarTurn == 1)
 		{
-			sonarF.computeDistance();
+			distanceR = sonarR.ping_cm();
 			sonarTurn = 2;
+			if (distanceR == 0)
+				return false;
+			else
+				return true;
 		}
-		if (sonarTurn == 2)
+		else if (sonarTurn == 2)
 		{
-			sonarR.computeDistance();
-			sonarTurn = 3;
-		}
-		if (sonarTurn == 3)
-		{
-			sonarL.computeDistance();
-			sonarTurn = 4;
-		}
-		if (sonarTurn == 4)
-		{
-			sonarFR.computeDistance();
-			sonarTurn = 5;
-		}
-		if (sonarTurn == 5)
-		{
-			sonarFL.computeDistance();
+			distanceFR = sonarFR.ping_cm();
 			sonarTurn = 1;
+			newSonar = true;
+			if (distanceFR == 0)
+				return false;
+			else
+				return true;
 		}
-		timingSonar = millis();
-		newSonar = true;
 	}
 }
 
-void Robot::printSonar()
+
+
+void Robot::printSonar(long distance[SONAR_NUM])
 {
-	Serial.print("sonarF: ");
-	Serial.println(sonarF.getDistance());
-	Serial.print("sonarL: ");
-	Serial.println(sonarL.getDistance());
-	Serial.print("sonarR: ");
-	Serial.println(sonarR.getDistance());
-	Serial.print("sonarFL: ");
-	Serial.println(sonarFL.getDistance());
-	Serial.print("sonarFR: ");
-	Serial.println(sonarFR.getDistance());
+	for (int i = 0; i < SONAR_NUM; i++)
+	{
+		Serial.print("Sensor ");
+		Serial.print(i);
+		Serial.print(": ");
+		Serial.print(distance[i]);
+		Serial.print("  ");
+	}
+	Serial.println();
+	//Serial.print("sonarF: ");
+	//Serial.println(sonarF.getDistance());
+	//Serial.print("sonarL: ");
+	//Serial.println(sonarL.getDistance());
+	//Serial.print("sonarR: ");
+	//Serial.println(sonarR.getDistance());
+	//Serial.print("sonarFL: ");
+	//Serial.println(sonarFL.getDistance());
+	//Serial.print("sonarFR: ");
+	//Serial.println(sonarFR.getDistance());
 }
+
+void Robot::newFollowWallRight(long distance[SONAR_NUM],
+	double desiredDistance, double vBase = 100.0)
+{
+	motorLeft.setMode(0);
+	motorRight.setMode(0);
+
+	double kp = 0.5;
+	double kd = 2.0;
+
+	double vLim = vBase / 4;
+	//bool isSucceed = actuateSonar();
+	long actualDistance = 0;
+	double deltaV = 0;
+	bool dismiss = false;
+
+	double error = 0;
+
+	if (distance[1] == 0)
+	{
+		if (distance[0] == 0) dismiss = true;
+		else actualDistance = distance[0];
+	}
+	else if (distance[0] == 0)
+	{
+		actualDistance = distance[1];
+	}
+	else actualDistance = min(distance[0], distance[1]);
+
+	printSonar(distance);
+	if (!dismiss)
+	{
+		error = desiredDistance - actualDistance;
+
+		deltaV = kp * error + kd * (error - errorOld);
+
+		if (deltaV > vLim) deltaV = vLim;
+		else if (deltaV < -vLim) deltaV = -vLim;
+
+		motorLeft.setVoltage(vBase - deltaV, 1);
+		motorRight.setVoltage(vBase + deltaV, 1);
+		Serial.print("  V Left: ");
+		Serial.print(vBase - deltaV);
+		Serial.print("  V Right: ");
+		Serial.println(vBase + deltaV);
+		errorOld = error;
+	}
+}
+
 
 void Robot::followWallRight(double desiredDistance, double vBase = 160.0)
 {
 	motorLeft.setMode(0);
 	motorRight.setMode(0);
 
-	double kp = 2.0;
+	double kp = 1.0;
 	double ki = 0.0;
 	double kd = 0.0;
 
-	double vLim = 80.0;
+	double vLim = vBase/2;
 	if (millis() > (timingSonar + tSonar))
 	{
 		newSonar = true;
 		timingSonar = millis();
 	}
+	bool dismiss = false;
 	if (newSonar == true)
 	{
-		int distanceLWall = newSonarR.ping_cm();
-		int distanceFLWall = newSonarFR.ping_cm();
-		Serial.print(distanceLWall);
-		Serial.print(" ");
-		Serial.print(distanceFLWall);
-		Serial.print(" ");
-		if (abs(distanceLWall - distanceFLWall) > desiredDistance)
+		if (sonarTurn == 1)
 		{
-			if (distanceLWall < distanceFLWall)
-				distanceFLWall = distanceLWall;
+			distanceLWall = sonarR.ping_cm();
+			sonarTurn = 2;
+		}
+		else if (sonarTurn == 2)
+		{
+			distanceFLWall = sonarFR.ping_cm();
+			sonarTurn = 1;
+		}
+		if (distanceLWall == 0)
+		{
+			if (distanceFLWall == 0)
+			{
+				dismiss = true;
+			}
 			else
+			{
 				distanceLWall = distanceFLWall;
+			}
 		}
-		double distanceWall =min(distanceFLWall, distanceLWall);
-		Serial.print(distanceWall);
-		Serial.print(" ");
-		double error = desiredDistance - distanceWall;
-		//Serial.print(error);
-		//Serial.print(" ");
-		double vPid = kp * error + (ki!=0)*vPidOld + ki * errorOld + kd * (error - errorOld);
-		Serial.print(vPid);
-		Serial.print(" ");
-		if (vPid > vLim)
+		if (distanceFLWall == 0)
 		{
-			vPid = vLim;
-			error = (vPid - vPidOld - ki * errorOld - kd * (error - errorOld)) / kp;
+			distanceFLWall = distanceLWall;
 		}
-		else if (vPid < -vLim)
+		if (!dismiss)
 		{
-			vPid = -vLim;
-			error = (vPid - vPidOld + ki * errorOld - kd * (error - errorOld)) / kp;
-		}
-		Serial.println(vPid);
-		vPidOld = vPid;
-		errorOld = error;
+			Serial.print(distanceLWall);
+			Serial.print(" ");
+			Serial.print(distanceFLWall);
+			Serial.print(" ");
+			//if (abs(distanceLWall - distanceFLWall) > desiredDistance)
+			//{
+			//	if (distanceLWall < distanceFLWall)
+			//		distanceFLWall = distanceLWall;
+			//	else
+			//		distanceLWall = distanceFLWall;
+			//}
+			double distanceWall = min(distanceFLWall, distanceLWall);
+			Serial.print(distanceWall);
+			Serial.print(" ");
+			double error = desiredDistance - distanceWall;
+			//Serial.print(error);
+			//Serial.print(" ");
+			double vPid = kp * error + (ki != 0)*vPidOld + ki * errorOld + kd * (error - errorOld);
+			Serial.print(vPid);
+			Serial.print(" ");
+			if (vPid > vLim)
+			{
+				vPid = vLim;
+				error = (vPid - (ki != 0) * vPidOld - ki * errorOld - kd * (error - errorOld)) / kp;
+			}
+			else if (vPid < -vLim)
+			{
+				vPid = -vLim;
+				error = (vPid - (ki != 0) * vPidOld + ki * errorOld - kd * (error - errorOld)) / kp;
+			}
+			Serial.println(vPid);
+			vPidOld = vPid;
+			errorOld = error;
 
-		double vLeftWheel = vBase - vPid;
-		double vRightWheel = vBase + vPid;
-		motorLeft.setVoltage(static_cast<int>(vLeftWheel),1);
-		motorRight.setVoltage(static_cast<int>(vRightWheel),1);
-		newSonar = false;
+			double vLeftWheel = vBase - vPid;
+			double vRightWheel = vBase + vPid;
+			motorLeft.setVoltage(static_cast<int>(vLeftWheel), 1);
+			motorRight.setVoltage(static_cast<int>(vRightWheel), 1);
+			newSonar = false;
+		}
 	}
 	
 }
@@ -327,12 +404,12 @@ void Robot::followWallLeftNewPing(double desiredDistance, int actualDistance,
 	if (vPid > vLim)
 	{
 		vPid = vLim;
-		error = (vPid - vPidOld - ki * errorOld - kd * (error - errorOld)) / kp;
+		error = (vPid - (ki != 0) * vPidOld - ki * errorOld - kd * (error - errorOld)) / kp;
 	}
 	else if (vPid < -vLim)
 	{
 		vPid = -vLim;
-		error = (vPid - vPidOld + ki * errorOld - kd * (error - errorOld)) / kp;
+		error = (vPid - (ki != 0) * vPidOld + ki * errorOld - kd * (error - errorOld)) / kp;
 	}
 	Serial.println(vPid);
 	vPidOld = vPid;
--- Robot.h
+++ Robot.h
@@ -1,7 +1,7 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
-
+#define SONAR_NUM 2
 class Robot {
 	Motor motorLeft{ 1, 18, 19 };
 	// The sampling time will be set later on
@@ -20,19 +20,23 @@ class Robot {
 	double speedOld;
 	double speedLeftOld = 0;
 	double speedRightOld = 0;
-	Sonar sonarR;
-	Sonar sonarL;
-	Sonar sonarF;
-	Sonar sonarFL;
-	Sonar sonarFR;
+	NewPing sonarR = NewPing(48, 49);
+	NewPing sonarL;
+	NewPing sonarF;
+	NewPing sonarFL;
+	NewPing sonarFR = NewPing(46, 47); 
 	int sonarTurn = 1;
 	long timingSonar;
 	int tSonar = 100;
 	double errorOld = 0;
 	double vPidOld = 0;
 	bool newSonar = false;
-	NewPing newSonarR = NewPing(48,49);
-	NewPing newSonarFR = NewPing(46, 47);
+	int distanceLWall;
+	int distanceFLWall;
+	long distanceR = 0;
+	long distanceFR = 0;
+	
+
 public:
 	Robot(int T = 50, double xPos = 0, double yPos = 0,
 		double omegaPos = 0, int base = 23, int diameter = 9);
@@ -41,17 +45,21 @@ public:
 	void stop();
 	double getSpeed() { return speed; }
 	void localisation();
-	void localisationSonar();
 	double getX() { return x; }
 	double getY() { return y; }
 	double getOmega() { return omega; }
 	bool goTo(double xDesired, double yDesired);
 	bool getAngle(double omegaDesired);
-	void actuateSonar();
-	void printSonar();
+	bool actuateSonar();
+	bool actuateSonarOptimized();
+	void printSonar(long distance[SONAR_NUM]);
+	void newFollowWallRight(long distance[SONAR_NUM], 
+		double desiredDistance, double vBase = 100.0);
 	void followWallRight(double desiredDistance, double vBase = 160.0);
 	void followWallLeftNewPing(double desiredDistance, int actualDistance,
 		double vBase = 100.0);
+
+
 };
 
 #endif
