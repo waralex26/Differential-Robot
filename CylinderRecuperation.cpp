acd7d54e43aff12e1b437831d41f65627d541909
waralex26 <38217365+waralex26@users.noreply.github.com>
2018-05-11 17:33:26 +0200

    Add files via upload

---
+++ CylinderRecuperation.cpp
@@ -0,0 +1,85 @@
+#include "Arduino.h"
+#include "CylinderRecuperation.h"
+
+
+bool CylinderRecuperation::s_isInterrupted{ false };
+
+void CylinderRecuperation::interrupted() {
+	
+	
+	//Serial.println(digitalRead(m_interruptPin));
+	if (digitalRead(m_interruptPin) == 1)
+	{
+		if (m_first)
+		{
+			m_isBlack = true;
+			m_first = false;
+		}
+		if (digitalRead(m_colorPin) == 1)
+		{
+			m_servoSort.write(m_sortWhiteAngle);
+			m_isBlack = false;
+			m_timing = millis();
+		}
+	}
+	else
+	{
+		if (m_isBlack)
+			++m_numberOfBlack;
+		if (m_numberOfBlack >= maxOfBlack)
+			m_isFull = true;
+		s_isInterrupted = false;
+		m_first = true;
+	}
+}
+
+void CylinderRecuperation::initialize(int servoSortPin,
+	int servoEmptyPin)
+{
+	m_servoSort.attach(servoSortPin);
+	m_servoSort.write(m_sortBlackAngle);
+	m_servoEmpty.attach(servoEmptyPin);
+	m_servoEmpty.write(m_closeDoor);
+}
+
+void CylinderRecuperation::loop()
+{
+	if (s_isInterrupted)
+		interrupted();
+
+	if (millis() > m_timing + 2000)
+		m_servoSort.write(m_sortBlackAngle);
+
+	if (m_isFull)
+		empty();
+
+	if (millis() > m_timingEmpty + 6000)
+		m_servoEmpty.write(m_closeDoor);
+
+	//Serial.println(m_numberOfBlack);
+}
+
+void CylinderRecuperation::empty()
+{
+	//Serial.println(m_isReached);
+	if (!m_isReached)
+	{
+		m_isReached = goToBase();
+		//Serial.println(m_isReached);
+	}
+	else
+	{
+		Serial.println(m_isReached);
+		m_servoEmpty.write(m_openDoor);
+		m_timingEmpty = millis();
+		m_isFull = false;
+		m_numberOfBlack = 0;
+		m_isReached = false;
+	}
+}
+
+bool CylinderRecuperation::goToBase()
+{
+	bool isReached = true;
+	return isReached;
+}
---
+++ CylinderRecuperation.h
@@ -0,0 +1,40 @@
+#ifndef CYLINDER_RECUPERATION
+#define CYLINDER_RECUPERATION
+
+#include "Servo.h"
+#include "Arduino.h"
+
+class CylinderRecuperation {
+private:
+	int m_interruptPin;
+	int m_colorPin;
+	Servo m_servoSort;
+	Servo m_servoEmpty;
+	int m_sortWhiteAngle = 90;
+	int m_sortBlackAngle = 50;
+	int m_openDoor = 80;
+	int m_closeDoor = 120;
+	int m_numberOfBlack = 0;
+	int maxOfBlack = 2;
+	bool m_isFull = false;
+	long m_timing = millis();
+	long m_timingEmpty = millis();
+	bool m_isReached = false;
+	bool m_isBlack;
+	bool m_first = true;
+public:
+	CylinderRecuperation(int servoSortPin = 5, int interruptPin = 2, int colorPin = 42)
+		:m_interruptPin(interruptPin), m_colorPin(colorPin)
+	{
+		//initialize(servoSortPin);
+	}
+	static bool s_isInterrupted;
+	void interrupted();
+	void initialize(int servoSortPin, int servoEmptyPin);
+	void loop();
+	void empty();
+	bool goToBase();
+};
+
+
+#endif
